df5eec94ce918aeb31ccad9d860069983eb3524b
Hogglo <zhoggard9@gmail.com>
2013-10-09 23:06:02 -0400

    added project files and resources

---
+++ FireAlertScanner/src/se3350y/aleph/firealertscanner/MainActivity.java
@@ -0,0 +1,33 @@
+package se3350y.aleph.firealertscanner;
+
+import android.os.Bundle;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+
+import com.dataInput.samplescanner.ScanCodeDemo;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	public void scanActionSelected(View view) {
+		// Create intent to call scanner activity
+		Intent intent = new Intent (this, ScanCodeDemo.class);
+		startActivity(intent);
+	}
+
+}
